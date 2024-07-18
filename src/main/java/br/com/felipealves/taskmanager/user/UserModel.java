df8027b62039a302c845246ca1f792fd52304be9
Felipe Alves <profelipealves@outlook.com>
2024-07-18 15:25:04 -0300

    feat: create UserModel.java

---
+++ src/main/java/br/com/felipealves/taskmanager/user/UserModel.java
@@ -0,0 +1,33 @@
+package br.com.felipealves.taskmanager.user;
+
+public class UserModel {
+
+    public String username;
+    public String name;
+    public String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
