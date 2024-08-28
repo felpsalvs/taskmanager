9b5b5ef875e3698065192b089666c623d9132c39
Felipe Alves <profelipealves@outlook.com>
2024-08-28 17:24:37 -0300

    validating username

--- src/main/java/br/com/felipealves/taskmanager/user/UserController.java
+++ src/main/java/br/com/felipealves/taskmanager/user/UserController.java
@@ -16,6 +16,13 @@ public class UserController {
 
     @PostMapping("/")
     public UserModel create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("Usuário existente!");
+            return null;
+        }
+
         var userCreated = this.userRepository.save(userModel);
         return userCreated;
     }
