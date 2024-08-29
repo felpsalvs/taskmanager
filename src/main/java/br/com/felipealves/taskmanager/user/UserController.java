a953eef056fff71e5b0a5967d20b886191462aa4
Felipe Alves <profelipealves@outlook.com>
2024-08-29 15:37:04 -0300

    using ResponseEntity

--- src/main/java/br/com/felipealves/taskmanager/user/UserController.java
+++ src/main/java/br/com/felipealves/taskmanager/user/UserController.java
@@ -2,6 +2,8 @@ package br.com.felipealves.taskmanager.user;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +17,14 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            System.out.println("Usuário existente!");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body:"usuario já existe");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
